32da7874cfa9f8d26a89c1dc5e71bab95a35cb31
Michael Kodiak <mkodiak@palantir.com>
2017-01-08 14:47:11 -0800

    Adding vscode configuration

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+// Hello world
+
+class App {
+    public static main(): boolean {
+        const message = "Hello world?";
+        console.log(message);
+        return true;
+    }
+}
+
+App.main();
