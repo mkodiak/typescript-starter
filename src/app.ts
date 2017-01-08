ea3bf13a207ab8061c00dbc3293d06129e651f62
Michael Kodiak <mkodiak@palantir.com>
2017-01-08 15:48:21 -0800

    Adding node

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 // Hello world
 
+import * as fs from "fs";
 import * as _ from "lodash";
 import { Print } from "./print";
 
@@ -7,6 +8,11 @@ class App {
     public static main(): boolean {
         const arr = ["Hello", "world", "!"];
         const message = _.join(arr, " ");
+
+        fs.readFile("./tslint.json", "utf8", (err, data) => {
+            Print.toConsole(data);
+        });
+
         Print.toConsole(message);
         return true;
     }
